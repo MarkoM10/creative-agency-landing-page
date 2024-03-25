d629184b046bc854eb7c49b4bd5e14d53c68e59a
MarkoM10 <58051779+MarkoM10@users.noreply.github.com>
2024-03-25 18:19:09 +0100

    Hero section

---
+++ script.js
@@ -0,0 +1,13 @@
+// function adjustContent() {
+//   const container = document.querySelector(".bg-heroMobile");
+//   if (window.innerWidth < 640) {
+//     container.style.cssText =
+//       "background-image: url('/assets/hero-mobile.jpg'); height: 800px; width: 890px;";
+//   } else {
+//     container.style.cssText =
+//       "background-image:  url('/assets/image-hero.jpg'); height: 300px; width: 890px;";
+//   }
+// }
+
+// window.addEventListener("load", adjustContent);
+// window.addEventListener("resize", adjustContent);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          100: "#ae3737",
+          200: "#c73f3f",
+          300: "#e04747",
+          400: "#f94f4f",
+          500: "#fa6161",
+          600: "#fa7272",
+          700: "#fb8484",
+          800: "#fb9595",
+          900: "#fca7a7",
+        },
+        dark: {
+          100: "#121217",
+          200: "#14141a",
+          300: "#17171e",
+          400: "#191921",
+          500: "#303037",
+          600: "#47474d",
+          700: "#5e5e64",
+          800: "#75757a",
+          900: "#8c8c90",
+        },
+      },
+      fontFamily: {
+        commisioner: ['"Commisioner"', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        hero: "url('/assets/image-hero.jpg')",
+        sectionOne: "url('/assets/image-strategic.jpg')",
+        heroMobile: "url('/assets/hero-mobile.jpg')",
+      },
+    },
   },
   plugins: [],
 };
